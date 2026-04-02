57dfd2264628630c925e0740fb904d07b86cb86f
jonataneckeskog <jonataneckeskog@gmail.com>
2026-04-02 15:01:48 +0200

    Implement mkPattern

--- A1N-chatterbot/student/src/Chatterbot.hs
+++ A1N-chatterbot/student/src/Chatterbot.hs
@@ -1,11 +1,13 @@
+-- Jonatan Eckeskog, Anders Persson
+
 module Chatterbot where
-import Utilities
-import System.Random
-import Data.Char
-import Data.Maybe
 
 -- If you're not sure what this is, it's ok.
 import Control.Monad (mapM)
+import Data.Char
+import Data.Maybe
+import System.Random
+import Utilities
 
 -- A pattern is a list of things
 -- Where we have either a value or a wildcard
@@ -29,8 +31,8 @@ type BotBrain = [Rule]
 
 chatterbot :: String -> [(String, [String])] -> IO ()
 chatterbot botName botRules = do
-    putStrLn ("\n\nHi! I am " ++ botName ++ ". How are you?")
-    botloop
+  putStrLn ("\n\nHi! I am " ++ botName ++ ". How are you?")
+  botloop
   where
     brain = rulesCompile botRules
     botloop = do
@@ -39,7 +41,6 @@ chatterbot botName botRules = do
       answer <- stateOfMind brain
       putStrLn (botName ++ ": " ++ (present . answer . prepare) question)
       if (not . endOfDialog) question then botloop else return ()
-
 
 --------------------------------------------------------
 
@@ -64,29 +65,28 @@ reflect :: Phrase -> Phrase
 reflect = undefined
 
 reflections =
-  [ ("am",     "are"),
-    ("was",    "were"),
-    ("i",      "you"),
-    ("i'm",    "you are"),
-    ("i'd",    "you would"),
-    ("i've",   "you have"),
-    ("i'll",   "you will"),
-    ("my",     "your"),
-    ("me",     "you"),
-    ("are",    "am"),
+  [ ("am", "are"),
+    ("was", "were"),
+    ("i", "you"),
+    ("i'm", "you are"),
+    ("i'd", "you would"),
+    ("i've", "you have"),
+    ("i'll", "you will"),
+    ("my", "your"),
+    ("me", "you"),
+    ("are", "am"),
     ("you're", "i am"),
     ("you've", "i have"),
     ("you'll", "i will"),
-    ("your",   "my"),
-    ("yours",  "mine"),
-    ("you",    "me")
+    ("your", "my"),
+    ("yours", "mine"),
+    ("you", "me")
   ]
-
 
 ---------------------------------------------------------------------------------
 
 endOfDialog :: String -> Bool
-endOfDialog = (=="quit") . map toLower
+endOfDialog = (== "quit") . map toLower
 
 present :: Phrase -> String
 present = unwords
@@ -103,13 +103,16 @@ ruleCompile = undefined
 
 --------------------------------------
 
-
 -- We can make a pattern from a list of elements
 -- If we choose one element that represents the wildcard
 -- mkPattern '*' "Hi *!" => [Item 'H', Item 'i', Wildcard, Item '!']
-mkPattern :: Eq a => a -> [a] -> Pattern a
+mkPattern :: (Eq a) => a -> [a] -> Pattern a
 {- TO BE WRITTEN -}
-mkPattern = undefined
+mkPattern _ [] = Pattern []
+mkPattern wc (x : xs) = Pattern (newItem : ps)
+  where
+    newItem = if x == wc then Wildcard else Item x
+    Pattern ps = mkPattern wc xs
 
 stringToPattern :: String -> String -> Pattern String
 stringToPattern wc = mkPattern wc . words
@@ -118,19 +121,21 @@ starPattern :: String -> Pattern String
 starPattern = stringToPattern "*"
 
 reductions :: [(Pattern String, Pattern String)]
-reductions = (map . map2) (starPattern, starPattern)
-  [ ( "please *", "*" ),
-    ( "could you *", "*" ),
-    ( "can you *", "*"),
-    ( "tell me if you are *", "are you *" ),
-    ( "tell me who * is", "who is *" ),
-    ( "tell me what * is", "what is *" ),
-    ( "do you know who * is", "who is *" ),
-    ( "do you know what * is", "what is *" ),
-    ( "are you very *", "are you *" ),
-    ( "i am very *", "i am *" ),
-    ( "hi *", "hello *")
-  ]
+reductions =
+  (map . map2)
+    (starPattern, starPattern)
+    [ ("please *", "*"),
+      ("could you *", "*"),
+      ("can you *", "*"),
+      ("tell me if you are *", "are you *"),
+      ("tell me who * is", "who is *"),
+      ("tell me what * is", "what is *"),
+      ("do you know who * is", "who is *"),
+      ("do you know what * is", "what is *"),
+      ("are you very *", "are you *"),
+      ("i am very *", "i am *"),
+      ("hi *", "hello *")
+    ]
 
 reduce :: Phrase -> Phrase
 reduce = reductionsApply reductions
@@ -139,47 +144,44 @@ reductionsApply :: [(Pattern String, Pattern String)] -> Phrase -> Phrase
 {- TO BE WRITTEN -}
 reductionsApply = undefined
 
-
 -------------------------------------------------------
 -- Match and substitute
 --------------------------------------------------------
 
 -- Replaces a wildcard in a template with the list given as the third argument
-substitute :: Eq a => Template a -> [a] -> [a]
+substitute :: (Eq a) => Template a -> [a] -> [a]
 {- TO BE WRITTEN -}
 substitute = undefined
 
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
-match :: Eq a => Pattern a -> [a] -> Maybe [a]
+match :: (Eq a) => Pattern a -> [a] -> Maybe [a]
 {- TO BE WRITTEN -}
 match = undefined
 
 -- Helper function to match
-singleWildcardMatch, longerWildcardMatch :: Eq a => Pattern a -> [a] -> Maybe [a]
-singleWildcardMatch (Pattern (Wildcard:ps)) (x:xs) =
+singleWildcardMatch, longerWildcardMatch :: (Eq a) => Pattern a -> [a] -> Maybe [a]
+singleWildcardMatch (Pattern (Wildcard : ps)) (x : xs) =
   case match (Pattern ps) xs of
     Nothing -> Nothing
     Just _ -> Just [x]
 {- TO BE WRITTEN -}
 longerWildcardMatch = undefined
 
-
-
 -------------------------------------------------------
 -- Applying patterns transformations
 --------------------------------------------------------
 
 -- Helper function: Matches a pattern and applies the transformation
-matchAndTransform :: Eq a => ([a] -> [a]) -> Pattern a -> [a] -> Maybe [a]
+matchAndTransform :: (Eq a) => ([a] -> [a]) -> Pattern a -> [a] -> Maybe [a]
 matchAndTransform transform pat = (mmap transform) . (match pat)
 
 -- Applying a single pattern
-transformationApply :: Eq a => ([a] -> [a]) -> [a] -> (Pattern a, Template a) -> Maybe [a]
+transformationApply :: (Eq a) => ([a] -> [a]) -> [a] -> (Pattern a, Template a) -> Maybe [a]
 {- TO BE WRITTEN -}
 transformationApply = undefined
 
 -- Applying a list of patterns until one succeeds
-transformationsApply :: Eq a => ([a] -> [a]) -> [(Pattern a, Template a)] -> [a] -> Maybe [a]
+transformationsApply :: (Eq a) => ([a] -> [a]) -> [(Pattern a, Template a)] -> [a] -> Maybe [a]
 {- TO BE WRITTEN -}
 transformationsApply = undefined
