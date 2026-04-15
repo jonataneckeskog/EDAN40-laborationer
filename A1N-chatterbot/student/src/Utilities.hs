280a029d650fe7b5d1f60b6e5d9fe71272068eb9
jonataneckeskog <jonataneckeskog@gmail.com>
2026-04-15 22:25:01 +0200

    Add comments to Utilities.hs

--- A1N-chatterbot/student/src/Utilities.hs
+++ A1N-chatterbot/student/src/Utilities.hs
@@ -4,6 +4,8 @@ map2 :: (a -> b, c -> d) -> (a, c) -> (b, d)
 map2 (f1, f2) (x1, x2) = (f1 x1, f2 x2)
 
 mmap :: (a -> b) -> Maybe a -> Maybe b
+-- Like a normal map, except we iterate over Maybe. When we hit Nothing we don't change it.
+-- If we hit a Just x we can actually apply the function f on x.
 mmap f  Nothing  = Nothing
 mmap f (Just x)  = Just (f x)
 
@@ -11,7 +13,10 @@ orElse :: Maybe a -> Maybe a -> Maybe a
 orElse Nothing  x  = x
 orElse (Just a) _  = Just a
 
+
 try :: (a -> Maybe a) -> a -> a
+-- We try to change x into a Just y using the function f. If the result is Nothing, we instead keep
+-- the original value x. Otherwise we extract the value y and return it.
 try f x = maybe x id (f x)
 
 fix :: Eq a => (a -> a) -> a -> a
