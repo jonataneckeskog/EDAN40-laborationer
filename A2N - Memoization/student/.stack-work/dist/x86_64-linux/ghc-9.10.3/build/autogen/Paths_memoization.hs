f840eb294bf91aea9043a3497b81550f91fc354f
jonataneckeskog <jonataneckeskog@gmail.com>
2026-04-29 11:35:39 +0200

    Run stack build + fibonacci

---
+++ A2N - Memoization/student/.stack-work/dist/x86_64-linux/ghc-9.10.3/build/autogen/Paths_memoization.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+#if __GLASGOW_HASKELL__ >= 810
+{-# OPTIONS_GHC -Wno-prepositive-qualified-module #-}
+#endif
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module Paths_memoization (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+
+import qualified Control.Exception as Exception
+import qualified Data.List as List
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir `joinFileName` name)
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+
+
+
+
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+bindir     = "/home/jonataneckeskog/Datateknik_LTH/Funktionell Programmering/EDAN40-laborationer/.stack-work/install/x86_64-linux/65926e05c0228b3f0fd67c515b60d67b6fce2688fd842490f580f88de1fb640e/9.10.3/bin"
+libdir     = "/home/jonataneckeskog/Datateknik_LTH/Funktionell Programmering/EDAN40-laborationer/.stack-work/install/x86_64-linux/65926e05c0228b3f0fd67c515b60d67b6fce2688fd842490f580f88de1fb640e/9.10.3/lib/x86_64-linux-ghc-9.10.3-b4c3/memoization-0.1.0.0-FBhB9TeAAMgJPoFD8SVLnN"
+dynlibdir  = "/home/jonataneckeskog/Datateknik_LTH/Funktionell Programmering/EDAN40-laborationer/.stack-work/install/x86_64-linux/65926e05c0228b3f0fd67c515b60d67b6fce2688fd842490f580f88de1fb640e/9.10.3/lib/x86_64-linux-ghc-9.10.3-b4c3"
+datadir    = "/home/jonataneckeskog/Datateknik_LTH/Funktionell Programmering/EDAN40-laborationer/.stack-work/install/x86_64-linux/65926e05c0228b3f0fd67c515b60d67b6fce2688fd842490f580f88de1fb640e/9.10.3/share/x86_64-linux-ghc-9.10.3-b4c3/memoization-0.1.0.0"
+libexecdir = "/home/jonataneckeskog/Datateknik_LTH/Funktionell Programmering/EDAN40-laborationer/.stack-work/install/x86_64-linux/65926e05c0228b3f0fd67c515b60d67b6fce2688fd842490f580f88de1fb640e/9.10.3/libexec/x86_64-linux-ghc-9.10.3-b4c3/memoization-0.1.0.0"
+sysconfdir = "/home/jonataneckeskog/Datateknik_LTH/Funktionell Programmering/EDAN40-laborationer/.stack-work/install/x86_64-linux/65926e05c0228b3f0fd67c515b60d67b6fce2688fd842490f580f88de1fb640e/9.10.3/etc"
+
+getBinDir     = catchIO (getEnv "memoization_bindir")     (\_ -> return bindir)
+getLibDir     = catchIO (getEnv "memoization_libdir")     (\_ -> return libdir)
+getDynLibDir  = catchIO (getEnv "memoization_dynlibdir")  (\_ -> return dynlibdir)
+getDataDir    = catchIO (getEnv "memoization_datadir")    (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "memoization_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "memoization_sysconfdir") (\_ -> return sysconfdir)
+
+
+
+joinFileName :: String -> String -> FilePath
+joinFileName ""  fname = fname
+joinFileName "." fname = fname
+joinFileName dir ""    = dir
+joinFileName dir fname
+  | isPathSeparator (List.last dir) = dir ++ fname
+  | otherwise                       = dir ++ pathSeparator : fname
+
+pathSeparator :: Char
+pathSeparator = '/'
+
+isPathSeparator :: Char -> Bool
+isPathSeparator c = c == '/'
